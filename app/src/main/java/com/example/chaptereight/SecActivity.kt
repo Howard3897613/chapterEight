52c37ee312b62c71495b280d505d1c91ea27b18d
陳庭浩 <112939670+Howard3897613@users.noreply.github.com>
2025-05-02 10:22:51 +0800

    calender added

--- app/src/main/java/com/example/chaptereight/SecActivity.kt
+++ app/src/main/java/com/example/chaptereight/SecActivity.kt
@@ -1,6 +1,7 @@
 package com.example.chaptereight
 
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -10,6 +11,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
+import java.util.jar.Attributes.Name
 
 class SecActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +27,11 @@ class SecActivity : AppCompatActivity() {
         val edName = findViewById<EditText>(R.id.edName)
         val edPhone = findViewById<EditText>(R.id.edPhone)
         val btnSend = findViewById<Button>(R.id.btnSend)
+        val btndate = findViewById<Button>(R.id.btnDate)
+        btndate.setOnClickListener{
+
+            val x = showDatePickerDialog(edPhone)
+        }
 
 
         // 設定按鈕監聽器，取得輸入的姓名與電話
@@ -47,5 +55,21 @@ class SecActivity : AppCompatActivity() {
     // 建立showToast方法顯示Toast訊息
     private fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+    private fun showDatePickerDialog(edPhone: EditText) {
+        // 獲取當前日期
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // 創建並顯示 DatePickerDialog
+
+        val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+            val selectedDate = "$selectedYear/${selectedMonth + 1}/$selectedDay"
+            edPhone.setText(selectedDate) // 更新 EditText 顯示選擇的日期
+        }, year, month, day)
+
+        datePickerDialog.show()
     }
 }
